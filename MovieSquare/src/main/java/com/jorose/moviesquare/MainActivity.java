97607b052f3e883526cd00662201f79e93eef6b6
Jdogg2K <jorose@gmail.com>
2013-08-20 12:42:39 -0400

    new oAuth, but not working

--- MovieSquare/src/main/java/com/jorose/moviesquare/MainActivity.java
+++ MovieSquare/src/main/java/com/jorose/moviesquare/MainActivity.java
@@ -1,111 +1,76 @@
 package com.jorose.moviesquare;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-
-import com.jorose.moviesquare.Foursquare.DialogListener;
-
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.TextUtils;
 import android.util.Log;
-import android.os.AsyncTask;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.foursquare.android.nativeoauth.FoursquareCancelException;
+import com.foursquare.android.nativeoauth.FoursquareDenyException;
+import com.foursquare.android.nativeoauth.FoursquareInvalidRequestException;
+import com.foursquare.android.nativeoauth.FoursquareOAuth;
+import com.foursquare.android.nativeoauth.FoursquareOAuthException;
+import com.foursquare.android.nativeoauth.FoursquareUnsupportedVersionException;
+import com.foursquare.android.nativeoauth.model.AccessTokenResponse;
+import com.foursquare.android.nativeoauth.model.AuthCodeResponse;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.impl.client.DefaultHttpClient;
-import java.io.InputStream;
-
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.json.JSONObject;
-import org.json.JSONException;
-import org.apache.http.client.methods.HttpGet;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
 
+
 public class MainActivity extends Activity {
 
-    Foursquare foursquare;
     String jsonResult;
     GetChildList childList;
+
+    private static final int REQUEST_CODE_FSQ_CONNECT = 200;
+    private static final int REQUEST_CODE_FSQ_TOKEN_EXCHANGE = 201;
+
+    /**
+     * Obtain your client id and secret from:
+     * https://foursquare.com/developers/apps
+     */
+    private static final String CLIENT_ID = "ZAJDKMJ13CTC25VCZFYTMQCV3YMMZXSQFUZIOEMUOVVRFQGG";
+    private static final String CLIENT_SECRET = "43T3FFM5KWHRXUP1M0BJVUV3ZJNAIYC2WVECDUJJJRHAG2EZ";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         //todo - TAKE THIS OFF AND DO background processing properly
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        childList = new GetChildList();
-        foursquare = new Foursquare(
-                "ZAJDKMJ13CTC25VCZFYTMQCV3YMMZXSQFUZIOEMUOVVRFQGG",
-                "43T3FFM5KWHRXUP1M0BJVUV3ZJNAIYC2WVECDUJJJRHAG2EZ",
-                "http://www.thejordanblog.com");
-
-        foursquare.authorize(this, new FoursquareAuthenDialogListener());
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    private class FoursquareAuthenDialogListener implements DialogListener {
-
-        @Override
-        public void onComplete(Bundle values) {
-            try {
-                String aa = null;
-                aa = foursquare.request("users/self");
-                Log.d("Foursquare-Main", aa);
-                childList.doInBackground();
-            } catch (MalformedURLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void onFoursquareError(FoursquareError e) {
-            // TODO Auto-generated method stub
-            String errorMe = e.toString();
-        }
-
-        @Override
-        public void onError(DialogError e) {
-            // TODO Auto-generated method stub
-            String errorMe = e.toString();
-        }
-
-        @Override
-        public void onCancel() {
-            // TODO Auto-generated method stub
-        }
-
+        ensureUi();
     }
 
     private class GetChildList extends AsyncTask<String, Void, String>{
 
         private String strm = "38.87,-77.23";
-        private String client_id = "ZAJDKMJ13CTC25VCZFYTMQCV3YMMZXSQFUZIOEMUOVVRFQGG";
-        private String client_secret = "43T3FFM5KWHRXUP1M0BJVUV3ZJNAIYC2WVECDUJJJRHAG2EZ";
         private String currentDateandTime = "20130715";  //yyyymmdd
 
         @Override
@@ -116,7 +81,7 @@ public class MainActivity extends Activity {
             HttpConnectionParams.setConnectionTimeout(httpParams, 30000);
             HttpConnectionParams.setSoTimeout(httpParams, 30000);
 
-            HttpGet httppost = new HttpGet("https://api.foursquare.com/v2/venues/search?intent=checkin&ll="+strm+"&client_id="+client_id+"&client_secret="+client_secret+"&v="+currentDateandTime+"&categoryId=4bf58dd8d48988d17f941735&limit=20"); //
+            HttpGet httppost = new HttpGet("https://api.foursquare.com/v2/venues/search?intent=checkin&ll="+strm+"&client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&v="+currentDateandTime+"&categoryId=4bf58dd8d48988d17f941735&limit=20"); //
 
             try{
 
@@ -227,4 +192,142 @@ public class MainActivity extends Activity {
         }
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    private void ensureUi() {
+        boolean isAuthorized = !TextUtils.isEmpty(ExampleTokenStore.get().getToken());
+
+        TextView tvTitle = (TextView) findViewById(R.id.viewTitle);
+        tvTitle.setVisibility(isAuthorized ? View.VISIBLE : View.GONE);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setVisibility(isAuthorized ? View.GONE : View.VISIBLE);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the native auth flow.
+                Intent intent = FoursquareOAuth.getConnectIntent(MainActivity.this, CLIENT_ID);
+
+                // If the device does not have the Foursquare app installed, we'd
+                // get an intent back that would open the Play Store for download.
+                // Otherwise we start the auth flow.
+                if (FoursquareOAuth.isPlayStoreIntent(intent)) {
+                    toastMessage(MainActivity.this, getString(R.string.app_not_installed_message));
+                    startActivity(intent);
+                } else {
+                    startActivityForResult(intent, REQUEST_CODE_FSQ_CONNECT);
+                }
+            }
+        });
+
+        if (isAuthorized){
+            childList = new GetChildList();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_FSQ_CONNECT:
+                onCompleteConnect(resultCode, data);
+                break;
+
+            case REQUEST_CODE_FSQ_TOKEN_EXCHANGE:
+                onCompleteTokenExchange(resultCode, data);
+                break;
+
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void onCompleteConnect(int resultCode, Intent data) {
+        AuthCodeResponse codeResponse = FoursquareOAuth.getAuthCodeFromResult(resultCode, data);
+        Exception exception = codeResponse.getException();
+
+        if (exception == null) {
+            // Success.
+            String code = codeResponse.getCode();
+            performTokenExchange(code);
+
+        } else {
+            if (exception instanceof FoursquareCancelException) {
+                // Cancel.
+                toastMessage(this, "Canceled");
+
+            } else if (exception instanceof FoursquareDenyException) {
+                // Deny.
+                toastMessage(this, "Denied");
+
+            } else if (exception instanceof FoursquareOAuthException) {
+                // OAuth error.
+                String errorMessage = exception.getMessage();
+                String errorCode = ((FoursquareOAuthException) exception).getErrorCode();
+                toastMessage(this, errorMessage + " [" + errorCode + "]");
+
+            } else if (exception instanceof FoursquareUnsupportedVersionException) {
+                // Unsupported Fourquare app version on the device.
+                toastError(this, exception);
+
+            } else if (exception instanceof FoursquareInvalidRequestException) {
+                // Invalid request.
+                toastError(this, exception);
+
+            } else {
+                // Error.
+                toastError(this, exception);
+            }
+        }
+    }
+
+    private void onCompleteTokenExchange(int resultCode, Intent data) {
+        AccessTokenResponse tokenResponse = FoursquareOAuth.getTokenFromResult(resultCode, data);
+        Exception exception = tokenResponse.getException();
+
+        if (exception == null) {
+            String accessToken = tokenResponse.getAccessToken();
+            // Success.
+            //toastMessage(this, "Access token: " + accessToken);
+
+            // Persist the token for later use. In this example, we save
+            // it to shared prefs.
+            ExampleTokenStore.get().setToken(accessToken);
+
+            // Refresh UI.
+            ensureUi();
+
+        } else {
+            if (exception instanceof FoursquareOAuthException) {
+                // OAuth error.
+                String errorMessage = ((FoursquareOAuthException) exception).getMessage();
+                String errorCode = ((FoursquareOAuthException) exception).getErrorCode();
+                toastMessage(this, errorMessage + " [" + errorCode + "]");
+
+            } else {
+                // Other exception type.
+                toastError(this, exception);
+            }
+        }
+    }
+
+    private void performTokenExchange(String code) {
+        Intent intent = FoursquareOAuth.getTokenExchangeIntent(this, CLIENT_ID, CLIENT_SECRET, code);
+        startActivityForResult(intent, REQUEST_CODE_FSQ_TOKEN_EXCHANGE);
+    }
+
+    public static void toastMessage(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void toastError(Context context, Throwable t) {
+        Toast.makeText(context, t.getMessage(), Toast.LENGTH_SHORT).show();
+    }
+
 }
